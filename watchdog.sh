bc26055c18b265638e39ab027cb9712b659f691d
Not.Config <83444054+Not-Config@users.noreply.github.com>
2026-09-13 00:35:02 +0300

    Watch web and mode-specific listeners

--- watchdog.sh
+++ watchdog.sh
@@ -14,25 +14,41 @@ container_running() {
 }
 
 tcp_listening() {
-  ss -lntH | grep -Eq "[:.]${VLESS_LISTEN_PORT}[[:space:]]"
+  local port="$1"
+  ss -lntH | grep -Eq "[:.]${port}[[:space:]]"
 }
 
 udp_listening() {
-  ss -lnuH | grep -Eq "[:.]${HY2_LISTEN_PORT}[[:space:]]"
+  local port="$1"
+  ss -lnuH | grep -Eq "[:.]${port}[[:space:]]"
 }
 
 if ! container_running vpn-xray; then
   logger -t vless-hysteria-watchdog 'vpn-xray is not running; starting it'
   docker compose up -d xray
-elif ! tcp_listening; then
-  logger -t vless-hysteria-watchdog "TCP/$VLESS_LISTEN_PORT is not listening; restarting vpn-xray"
+elif [[ "$VLESS_MODE" == "reality" ]] && ! tcp_listening "$VLESS_LISTEN_PORT"; then
+  logger -t vless-hysteria-watchdog "TCP/$VLESS_LISTEN_PORT is not listening for REALITY; restarting vpn-xray"
   docker restart vpn-xray >/dev/null
+elif [[ "$VLESS_MODE" == "web-grpc" ]] && ! tcp_listening "$VLESS_GRPC_BACKEND_PORT"; then
+  logger -t vless-hysteria-watchdog "gRPC backend TCP/$VLESS_GRPC_BACKEND_PORT is not listening; restarting vpn-xray"
+  docker restart vpn-xray >/dev/null
+fi
+
+if ! container_running vpn-web; then
+  logger -t vless-hysteria-watchdog 'vpn-web is not running; starting it'
+  docker compose up -d web
+elif ! tcp_listening "$WEB_LOCAL_PORT"; then
+  logger -t vless-hysteria-watchdog "Local website TCP/$WEB_LOCAL_PORT is not listening; restarting vpn-web"
+  docker restart vpn-web >/dev/null
+elif [[ "$VLESS_MODE" == "web-grpc" ]] && ! tcp_listening "$VLESS_LISTEN_PORT"; then
+  logger -t vless-hysteria-watchdog "Public web/VLESS TCP/$VLESS_LISTEN_PORT is not listening; restarting vpn-web"
+  docker restart vpn-web >/dev/null
 fi
 
 if ! container_running vpn-hysteria; then
   logger -t vless-hysteria-watchdog 'vpn-hysteria is not running; starting it'
   docker compose up -d hysteria
-elif ! udp_listening; then
+elif ! udp_listening "$HY2_LISTEN_PORT"; then
   logger -t vless-hysteria-watchdog "UDP/$HY2_LISTEN_PORT is not listening; restarting vpn-hysteria"
   docker restart vpn-hysteria >/dev/null
 fi
