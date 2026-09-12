a44df3bc7bf9400e6500f9ddadc9123bc2ed59af
Not.Config <83444054+Not-Config@users.noreply.github.com>
2026-09-12 22:45:14 +0300

    fix: read optional installer variables safely

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ if [[ -e "$VH_HOME/.env" && $FORCE -ne 1 ]]; then
 fi
 
 prompt_value() {
-  local var="$1" text="$2" default="$3" value="${!var:-}"
+  local var="$1" text="$2" default="$3" value="${!var-}"
   if [[ -z "$value" ]]; then
     if [[ $NON_INTERACTIVE -eq 1 ]]; then
       value="$default"
